07278aa49b767797e41ca5d32da0f25b406e5d75
nenahui <kanatraccoon@gmail.com>
2024-09-11 15:53:33 +0600

    Created config and added theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { ConfigProvider } from 'antd';
 import React from 'react';
+import { config } from './app/config/config';
 import { Routes } from './app/routes/routes';
 
 export const App: React.FC = () => {
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: '#13c2c2',
-        },
-      }}
-    >
+    <ConfigProvider theme={config.theme}>
       <Routes />
     </ConfigProvider>
   );
---
+++ src/app/config/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  theme: {
+    token: {
+      colorPrimary: '#13c2c2',
+    },
+  },
+};
