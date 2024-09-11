720a5645d0f94721aacd5d243b1126a9b69247ed
nenahui <kanatraccoon@gmail.com>
2024-09-11 17:27:41 +0600

    Header improved

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -2,11 +2,12 @@ import { Navigation } from '@/entities/navigation';
 import { Logo } from '@/shared/ui/logo';
 import { Button, Flex } from 'antd';
 import React from 'react';
+import styles from './header.module.scss';
 
 export const Header: React.FC = () => {
   return (
     <div className={'container mx-auto mt-4'}>
-      <Flex justify={'space-between'} align={'center'}>
+      <Flex justify={'space-between'} align={'center'} className={styles.header}>
         <Logo />
         <Navigation />
         <Button type={'primary'} shape={'round'} size={'large'}>
