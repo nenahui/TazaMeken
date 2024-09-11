8a29fda89ef86aaf0e51050e9677243be5332bed
IskenderDev <iskenderkadyrjanov@gmail.com>
2024-09-11 16:13:38 +0600

    created folders

---
+++ src/entities/sponsers/ui/Sponsers.tsx
@@ -0,0 +1,11 @@
+import { Flex } from "antd"
+
+const Sponsers = () => {
+  return (
+    <Flex>
+        
+    </Flex>
+  )
+}
+
+export default Sponsers
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { App } from './app/app';
 import './app/index.scss';
+import { App } from '@/app/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
