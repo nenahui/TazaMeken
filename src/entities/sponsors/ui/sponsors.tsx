d7c0c689725a5930060e6affcd5f2044e3dc77a7
IskenderDev <iskenderkadyrjanov@gmail.com>
2024-09-11 16:56:30 +0600

    added prim color

--- src/entities/sponsors/ui/sponsors.tsx
+++ src/entities/sponsors/ui/sponsors.tsx
@@ -1,9 +1,19 @@
 import { Flex } from 'antd';
+import { Typography } from 'antd';
+
+const { Text } = Typography;
 
 const Sponsors = () => {
   return (
-    <Flex>
-      <p>q</p>
+    <Flex className='container mx-20 size-96' gap={20} vertical>
+      <Flex className='container mx-auto' vertical>
+        <Text className='text-2xl'>Количество спонсоров</Text>
+        <Text className='text-primary-color font-medium text-7xl'>40+</Text>
+      </Flex>
+      <Text className='text-2xl'>
+        Создано с заботой об экологии, чтобы улучшить вашу жизнь уже сегодня и создать гармоничное, устойчивое будущее
+        для вас и ваших близких.
+      </Text>
     </Flex>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './app/index.scss';
 import { App } from '@/app/app';
+import '@/app/styles/index.scss';
 
 
 createRoot(document.getElementById('root')!).render(
